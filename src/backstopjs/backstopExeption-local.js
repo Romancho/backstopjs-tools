c01d0640e50ecdbfa1853e1fda6e52a01ee8b79d
Romancho <dev@romancho.pro>
2019-10-15 06:36:32 +0000

    Add external exeptions from module

--- src/backstopjs/backstopExeption-local.js
+++ src/backstopjs/backstopExeption-local.js
@@ -1,3 +1,16 @@
+const clc = require('cli-color');
+const fs = require('fs');
+const path = require('path');
+
+let configData = fs.readFileSync(path.resolve(__dirname, '../../config.json'));
+let config = JSON.parse(configData);
+let moduleExeption;
+
+if (config.module !== "") {
+    moduleExeption =  fs.readFileSync(path.resolve(__dirname, '../../node_modules/' + config.module + '/backstooExeption';
+}
+
 module.exports = [
+    moduleExeption,
     ".disclamer"
 ];
