25df2352bd0125f58bd660acb9d7cf2fe781133b
Roman Zhukov <dev@romancho.pro>
2019-10-17 13:00:57 +0300

    Task: #0. Пример частного конфига + правка шаблона конфига.

--- src/backstopjs/test-local/separate-local.js
+++ src/backstopjs/test-local/separate-local.js
@@ -5,11 +5,31 @@ let breakpoints = require('../backstopViewports-local');
 
 const reference_exeption = require(path.resolve(__dirname, '../backstopExeption-local'));
 
+// Пример локального сценария
 
+// const social = {                                       // Правим название константы (должно быть уникально)
+//     'label': 'social',                                 // Правим название сценария (используется в консоли как --filter=social)
+//     'cookiePath': cookieFile,
+//     'referenceUrl': urlReference + '/about',           // Добавляем урл нужной страницы, которую скриним
+//     'url': urlTesting + '/about',                      // Дублируем урл нужной страницы, которую тестим
+//     'readyEvent': '',
+//     'readySelector': '',
+//     'delay': 1000,
+//     'removeSelectors': [],                             // Тут селектор в виде '.selector', которые исключаем (при использовании bs-конфига есть предопределeнный набор reference_exeption)
+//     'hoverSelector': '',                               // Тут селектор в виде '.selector', если на него надо навести
+//     'clickSelector': '',                               // Тут селектор в виде '.selector', если на него надо кликнуть
+//     'selectors' : [
+//         '.subscriber'                                  // Тут селектор в виде '.selector', какой мы проверяем
+//     ],
+//     'hideSelectors': [],
+//     'viewports': [                                    // Брейкпоинты, если нужно проверять только в определенном разрешении
+//         breakpoints.ipad,
+//         breakpoints.desktop
+//     ]
+// };
 
 const scenarios = [
-    //social,
-    //authPop
+    //social,                                            // Вызов константы
 ];
 
 const basicTestSettings = {
