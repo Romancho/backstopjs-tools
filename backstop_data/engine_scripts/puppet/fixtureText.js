f165b06d5a9ae1df6e1128684c4231513d3924ab
Roman Zhukov <dev@romancho.pro>
2019-11-01 10:16:52 +0300

    Task: #0. Не рабочий вариант

--- backstop_data/engine_scripts/puppet/fixtureText.js
+++ backstop_data/engine_scripts/puppet/fixtureText.js
@@ -1,8 +1,22 @@
-module.exports = async (page, scenario, vp) => {
+module.exports = async (page) => {
+    const myLocalValue = "test 123";
     const tweets = await page.$$('.menu__link');
 
-    const myLocalValue = "test 123";
+    function getText(linkText) {
+        linkText = linkText.replace(/\r\n|\r/g, "\n");
+        linkText = linkText.replace(/\ +/g, " ");
+
+        // Replace &nbsp; with a space
+        var nbspPattern = new RegExp(String.fromCharCode(160), "g");
+        return linkText.replace(nbspPattern, " ");
+    }
+
     for (let i = 0; i < tweets.length; i++) {
-        await page.$eval(tweets[i], (el, value) => el.innerText = value, myLocalValue)
+        let valueHandle = await tweets[i].getProperty('innerText');
+        let linkText = await valueHandle.jsonValue();
+        //await page.$eval(tweets[i], (el, value) => el.innerHTML = value, myLocalValue)
+        const text = getText(linkText);
+        console.log(text);
+        return tweets[i];
     }
 };
