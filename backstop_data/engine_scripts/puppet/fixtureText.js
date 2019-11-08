4ef18eca7feb7ab6ea601bc8d6443ed0ffc5cf6e
Roman Zhukov <dev@romancho.pro>
2019-11-08 11:13:29 +0300

    Task: #0. Продолжение мучений.

--- backstop_data/engine_scripts/puppet/fixtureText.js
+++ backstop_data/engine_scripts/puppet/fixtureText.js
@@ -1,7 +1,9 @@
 module.exports = async (page) => {
     const myLocalValue = "Test";
     const tweets = await page.$$('.menu__link');
-    for (let i = 0; i < tweets.length; i++) {
-        await page.$eval('.menu__link', (el, value) => el.innerHTML = value, myLocalValue)
+
+
+    for (const tweet of tweets) {
+        await page.$eval(tweet, (el, value) => el.innerHTML = value, myLocalValue)
     }
 };
