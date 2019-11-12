74a902085c7ab93551afb17230ce25d0d38c6850
Roman Zhukov <dev@romancho.pro>
2019-11-12 16:07:48 +0300

    Task: #0. Fix exception concate.

--- src/backstopjs/backstopExeption-local.js
+++ src/backstopjs/backstopExeption-local.js
@@ -3,11 +3,15 @@ const path = require('path');
 
 let configData = fs.readFileSync(path.resolve(__dirname, '../../config.json'));
 let config = JSON.parse(configData);
-let moduleExeptions = [];
+let moduleExeptions = [
+    '.announce-category'
+];
 
 if (config.module !== "") {
-    moduleExeptions =  require(path.resolve(__dirname, '../../node_modules/' + config.module + '/backstopExeption.js'));
+    let moduleExeptionsExport =  require(path.resolve(__dirname, '../../node_modules/' + config.module + '/backstopExeption.js'));
+    moduleExeptions = moduleExeptions.concat(moduleExeptionsExport);
 }
+
 module.exports = [
     moduleExeptions
 ];
