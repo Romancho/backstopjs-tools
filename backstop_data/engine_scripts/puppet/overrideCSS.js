00c9449d21001b7b6300e814b419c4e69b7b8183
Romancho <dev@romancho.pro>
2020-03-06 14:46:20 +0300

    Task: #0. Update mech bs-tools.

--- backstop_data/engine_scripts/puppet/overrideCSS.js
+++ backstop_data/engine_scripts/puppet/overrideCSS.js
@@ -1,4 +1,20 @@
-const BACKSTOP_TEST_CSS_OVERRIDE = 'html {background-image: none;} .bs-bgcolor {background: #DFFFF9!important;}';
+const fs = require('fs');
+const path = require('path');
+let clc = require("cli-color");
+
+let configData = fs.readFileSync(path.resolve(__dirname, '../../../bs-config.json'));
+let config = JSON.parse(configData);
+
+let BACKSTOP_TEST_CSS_OVERRIDE = '';
+
+if (config.module.length > 0) {
+    BACKSTOP_TEST_CSS_OVERRIDE = require('../../../node_modules/bs-playboy/tests/overrideCss.js');
+    if (BACKSTOP_TEST_CSS_OVERRIDE === undefined) {
+        console.log(clc.pink('NO CSS OVERRIDE FILE'));
+    }
+} else {
+    BACKSTOP_TEST_CSS_OVERRIDE = 'html {background-image: none;} .bs-bgcolor {background: #DFFFF9!important;}';
+}
 
 module.exports = function (page, scenario) {
     // inject arbitrary css to override styles
@@ -6,7 +22,7 @@ module.exports = function (page, scenario) {
     page.evaluate(() => {
         var style = document.createElement('style');
         style.type = 'text/css';
-        var styleNode = document.createTextNode(window._styleData);
+        var styleNode = document.createTextNode(window._styleData);Task: #
         style.appendChild(styleNode);
         document.head.appendChild(style);
     });
